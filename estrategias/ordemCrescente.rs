f05d9e902bc697298822db673698f385e4c22e61
Gustavo dos Santos Leão <102435052+agatrix@users.noreply.github.com>
2025-06-17 14:28:19 -0300

    Add ordemCrescente.rs
    Um arquivo que possui uma função que ordena um vetor de inteiros de 64 bits, com o método de Insertion Sort.
    Recebe como parâmetro um Vec<i64> e não possui nenhum retorno.

---
+++ estrategias/ordemCrescente.rs
@@ -0,0 +1,11 @@
+pub fn ordemCrescente(lista: &mut Vec<i64>) {
+    let n = lista.len();
+    for i in 1..n {
+        let mut j = i;
+        while j > 0 && lista[j - 1] > lista[j] {
+            lista.swap(j, j - 1);
+            j -= 1;
+        }
+
+    }
+}
