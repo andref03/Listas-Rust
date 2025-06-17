c5cae5fbdee2957ef12d5f072bec63a9bd7e2e44
BeruasCS <108592252+BeruasCS@users.noreply.github.com>
2025-06-17 20:20:28 -0300

    atualização da main.rs

--- main.rs
+++ main.rs
@@ -9,6 +9,6 @@ fn main() {
     let mut numeros : Vec<i64> = vec![5, 5, 1, 0, 9, 8, 6, 2, 8, 5, 2, 9, 1, 0, 4, 4];
     println!("Lista original: {:?}", numeros);
 
-    executar_estrategia(&mut numeros, estrategias::ordemCrescente::ordemCrescente);
+    executar_estrategia(&mut numeros, estrategias::ordena_decrescente::ordena_decrescente);
     
 }
