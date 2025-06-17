bde738cc1294320af00e8eba47ef039b246cf46d
Gustavo dos Santos Leão <102435052+agatrix@users.noreply.github.com>
2025-06-17 14:45:37 -0300

    Correção main.rs
    Foi corrigido a main, para reconhecer os módulos dentro da pasta "estrategias", e corrigido a chamada da função "executar_estrategia" na main.

--- main.rs
+++ main.rs
@@ -1,4 +1,4 @@
-mod ordemCresnte;
+mod estrategias;
 
 fn executar_estrategia(lista: &mut Vec<i64>, estrategia: fn(&mut Vec<i64>)) {
     estrategia(lista);
@@ -9,6 +9,6 @@ fn main() {
     let mut numeros : Vec<i64> = vec![5, 5, 1, 0, 9, 8, 6, 2, 8, 5, 2, 9, 1, 0, 4, 4];
     println!("Lista original: {:?}", numeros);
 
-    executar_estrategia(&mut numeros, ordemCrescente);
+    executar_estrategia(&mut numeros, estrategias::ordemCrescente::ordemCrescente);
     
 }
