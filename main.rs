c98e48fd1c1813911659283d5bd1006c07b138c7
Gustavo dos Santos Leão <102435052+agatrix@users.noreply.github.com>
2025-06-17 14:25:55 -0300

    Alteração Main (ordemCrescente)
    Foi adicionado a função de executar as estratégias que possui um ponteiro de função para a chamada da função ordemCrescente. E dentro da main foi adicionado a chamada para a função que executa as estratégias.

--- main.rs
+++ main.rs
@@ -1,6 +1,14 @@
+mod ordemCresnte;
+
+fn executar_estrategia(lista: &mut Vec<i64>, estrategia: fn(&mut Vec<i64>)) {
+    estrategia(lista);
+    println!("Resultado: {:?}", lista);
+}
 
 fn main() {
     let mut numeros : Vec<i64> = vec![5, 5, 1, 0, 9, 8, 6, 2, 8, 5, 2, 9, 1, 0, 4, 4];
     println!("Lista original: {:?}", numeros);
 
+    executar_estrategia(&mut numeros, ordemCrescente);
+    
 }
